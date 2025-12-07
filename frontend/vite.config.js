7d2b03796c01e4fde7d29e56c5205ffa52e16f9a
Ivan Jaque <ijaquepinto@gmail.com>
2025-12-07 17:04:20 -0300

    fix: Corrección para lograr hacer deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/medical-sync-dashboard/', // Nombre de tu repositorio
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
